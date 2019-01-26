440951a67506af382cf4b1b6b06517d48c50296e
Wanna HK <donoseoana@gmail.com>
2019-01-26 16:02:07 +0000

    added user and profile models + login/register validation

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://uana:uana123@ds111425.mlab.com:11425/devconnector"
+  mongoURI: "mongodb://uana:uana123@ds111425.mlab.com:11425/devconnector",
+  secretOrKey: "secret"
 };
